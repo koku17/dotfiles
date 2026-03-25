151819f310a57582dce4c8dbd05ba9e38e49d4ea
koku17 <koku17@nomail.com>
2026-03-25 20:58:53 +0530

    Wednesday 25 March 2026 08:58:53 PM

---
+++ .config/swayimg/init.lua
@@ -0,0 +1,8 @@
+swayimg.text.set_size(10)
+swayimg.text.set_foreground(0xffff0000)
+
+swayimg.viewer.set_default_scale("fill")
+
+--swayimg.gallery.on_key("Delete", function()
+--  local image = swayimg.gallery.get_image()
+--=end)
